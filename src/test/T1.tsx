3a7fc3f91e132c1c05188b18415bd998aef22a1a
JaneGe <625806758@qq.com>
2019-01-15 23:49:08 +0800

    create a react product project goods

---
+++ src/test/T1.tsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class T1 extends Component{
+    render(){
+        return (
+            <div>
+                T1 Test
+            </div>
+        );
+    }
+}
+export default T1;
